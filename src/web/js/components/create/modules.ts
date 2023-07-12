427f0b29fa59bd53a820a3846d73f2991f55dca3
Allybe <creeperplay1212@gmail.com>
2023-07-12 15:00:15 -0500

    Fixed Git Ignore

--- src/web/js/components/create/modules.ts
+++ src/web/js/components/create/modules.ts
@@ -1,1 +1,14 @@
-export const component: Command = {};
+import { Command } from "../../interfaces/component.js";
+
+export const component: Command = {
+  targetId: "none",
+  elementType: "none",
+  data: () => {
+    return null;
+  },
+  methods: {
+    onclick: function () {
+      console.log("clicked");
+    },
+  },
+};

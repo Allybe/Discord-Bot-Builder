02b22f74d8667ccb8005ebf4eb491294fd6b836c
Allison <67673392+Allybe@users.noreply.github.com>
2026-02-02 19:18:53 -0600

    lots of progress with the UI, done for today :3

---
+++ DiscordBotBuilder/App.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Discord_Bot_Builder;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }   
+}
